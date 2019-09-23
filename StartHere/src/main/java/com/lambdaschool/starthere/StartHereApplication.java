3ad2ffc5a668a0f06ee2cec9638ddf00ac6fddf0
Kenya-a <Kenya-a@users.noreply.github.com>
2019-09-23 16:04:27 -0500

    initial commit

--- StartHere/src/main/java/com/lambdaschool/starthere/StartHereApplication.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/StartHereApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ApplicationObjectSupport;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.DispatcherServlet;
